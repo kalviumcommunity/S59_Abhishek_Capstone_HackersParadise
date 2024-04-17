13ddd2a4239140bcf421ea33f9802ccfa3dde48e
abhishekkaundal0052 <kaundal.abhishek0052@gmail.com>
2024-04-17 17:39:40 +0530

    Joi validation added to the Schema

--- server/routes.js
+++ server/routes.js
@@ -3,7 +3,27 @@ const router = express.Router();
 const { connectDB } = require('./db.js')
 const user = require('./Schemas/userSchema.js')
 
+const validateLogin = Joi.object({
+    fname: Joi.string().required(),
+    lname: Joi.string(),
+    mail: Joi.string().required(),
+    password: Joi.string().required(),
+})
+
+const checkValidation = (input, schema) => {
+    const { error } = schema.validate(input)
+    if (error) {
+        return false
+    }
+    else {
+        return true
+    }
+}
+
 router.post('/register', async (req, res) => {
+    if (!checkValidation(req.body, validateLogin)) {
+        return res.status(400).json({ "Error": "Data validation failed. Please add data as per the norms" })
+    }
     const newUser = new user({
         fname: req.body.fname,
         lname: req.body.lname,
