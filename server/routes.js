66c579044307081ccfe38164d1090f9955323db3
Abhishek Kaundal <144792445+AbhishekKaundal0052@users.noreply.github.com>
2024-04-19 17:34:16 +0530

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router();
+const Joi = require('joi');
 const { connectDB } = require('./db.js')
 const user = require('./Schemas/userSchema.js')
 
