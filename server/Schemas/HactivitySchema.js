e99a16922b1c7701d8ed97306f1fd0dfe611b7fd
abhishekkaundal0052 <kaundal.abhishek0052@gmail.com>
2024-05-08 17:40:43 +0530

    Schema and post router added

---
+++ server/Schemas/HactivitySchema.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const HactivitySchema = new mongoose.Schema({
+    topic:{
+        type : String,
+        required : true
+    },
+    level : {
+        type: String,
+        required : true
+    },
+    info : {
+        type : String,
+        required : true
+    },
+    tags : {
+        type : String
+    },
+    bountyreward : {
+        type: String
+    }
+})
+
+const Hactivity = mongoose.model('bounty', HactivitySchema)
+module.exports = Hactivity
