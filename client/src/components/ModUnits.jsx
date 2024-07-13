e07907f4daaf063493d5bdda9e9fd7d17f9dc0c5
abhishekkaundal0052 <kaundal.abhishek0052@gmail.com>
2024-07-13 15:59:06 +0530

    design

--- client/src/components/ModUnits.jsx
+++ client/src/components/ModUnits.jsx
@@ -6,7 +6,7 @@ import {Link} from 'react-router-dom'
 export default function ModUnits() {
   return (
     <div className="bg-[#000746] h-full">
-      <h1>1. NETWORKING</h1>
+      <h1 className="text-[4vw] text-white ml-[2vw]">1. NETWORKING</h1>
       <div className="pt-[4vh] pb-[4vh]">
         <div className="m-auto p-[0.2rem] bg-gradient-to-r from-[#d48ff9] via-[#b25ffb] to-[#6300ff] rounded-[0.9rem] w-[40vw]">
           <div className="flex justify-between bg-[#000746] p-[0.2rem] pr-[1rem] pl-[1rem] rounded-xl">
