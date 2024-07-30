13cc072ab9d9dfcb85b1ed206156eccc17363960
abhishekkaundal0052 <kaundal.abhishek0052@gmail.com>
2024-07-30 09:14:23 +0530

    Made changes in UI

--- client/src/components/ModUnits.jsx
+++ client/src/components/ModUnits.jsx
@@ -21,24 +21,24 @@ export default function ModUnits() {
 
   return (
     <div className="bg-[#000746] h-full">
-      <div className="pt-[4vh] pb-[4vh]">
-        <div className="m-auto p-[0.2rem] bg-gradient-to-r from-[#d48ff9] via-[#b25ffb] to-[#6300ff] rounded-[0.9rem] w-[40vw]">
-          <div className="flex justify-between bg-[#000746] p-[0.2rem] pr-[1rem] pl-[1rem] rounded-xl">
-            <input
-              placeholder="Search Modules here..."
-              className="text-[#d48ff9] placeholder-[#d48ff9] bg-[#000746] text-[1vw] w-full focus:outline-none focus:ring-0 font-semibold"
-            ></input>
-            <img
-              src={search}
-              alt="search Icon"
-              className="cursor-pointer w-[2vw]"
-            ></img>
-          </div>
-        </div>
-      </div>
       {data.map((unit) => (
-        <div key={unit.unitName}>
+        <div key={unit.unitName} className="m-[7vw]">
           <h1 className="text-[4vw] text-white ml-[2vw]">{unit.unitName}</h1>
+          <div className="pt-[4vh] pb-[4vh] flex justify-items-start">
+            <div className="p-[0.2rem] bg-gradient-to-r from-[#d48ff9] via-[#b25ffb] to-[#6300ff] rounded-[0.9rem] w-[40vw]">
+              <div className="flex justify-between bg-[#000746] p-[0.2rem] pr-[1rem] pl-[1rem] rounded-xl">
+                <input
+                  placeholder="Search Modules here..."
+                  className="text-[#d48ff9] placeholder-[#d48ff9] bg-[#000746] text-[1vw] w-full focus:outline-none focus:ring-0 font-semibold"
+                ></input>
+                <img
+                  src={search}
+                  alt="search Icon"
+                  className="cursor-pointer w-[2vw]"
+                ></img>
+              </div>
+            </div>
+          </div>
           <div className="flex flex-wrap w-[50vw]">
             {unit.names.map((name, index) => (
               <Link to="/Unit">
@@ -46,8 +46,12 @@ export default function ModUnits() {
                   key={index}
                   className="bg-[#9f54ff] w-[50vw] m-auto mb-[5vh] text-white flex justify-between rounded-xl"
                 >
-                  <div className="p-6">
-                    <p className="text-[1.5rem] font-semibold">{name}</p>
+                  <div className="bg-[#8e35ff] p-4 rounded-xl flex items-center w-[20%]">
+                    <div className="flex flex-col items-center mr-4">
+                      <div className="p-6">
+                        <p className="text-[1.5rem] font-semibold">{name}</p>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </Link>
